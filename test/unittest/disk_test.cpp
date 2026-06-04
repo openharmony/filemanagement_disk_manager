39a70298035bbbd23e439c9dfd70e5a7c78b333c
zhonglufu <zhonglufu@h-partners.com>
2026-06-04 15:52:45 +0800

    test: add UT for disk, volume_core, volume_external and partition_types
    Add 4 independent UT test files covering all methods and branches for innerkits data model classes. Each test is a standalone ohos_unittest target for independent compilation.
    
    Co-Authored-By: Agent
    Signed-off-by: zhonglufu <zhonglufu@h-partners.com>

---
+++ test/unittest/disk_test.cpp
@@ -0,0 +1,440 @@
+/*
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include "disk.h"
+#include "message_parcel_mock.h"
+
+namespace OHOS {
+namespace DiskManager {
+
+using namespace testing;
+using namespace testing::ext;
+
+class DiskTest : public testing::Test {
+public:
+    static void SetUpTestCase(void) {}
+    static void TearDownTestCase(void) {}
+    void SetUp() override {}
+    void TearDown() override {}
+};
+
+HWTEST_F(DiskTest, DefaultConstructor_TestCase_001, TestSize.Level0)
+{
+    Disk disk;
+    EXPECT_EQ(disk.GetDiskId(), "");
+    EXPECT_EQ(disk.GetSizeBytes(), 0);
+    EXPECT_EQ(disk.GetDiskType(), DISK_TYPE_UNKNOWN);
+    EXPECT_TRUE(disk.GetRemovable());
+    EXPECT_TRUE(disk.IsRemovable());
+    EXPECT_EQ(disk.GetSysPath(), "");
+    EXPECT_FALSE(disk.IsInternalDataDisk());
+}
+
+HWTEST_F(DiskTest, ParameterizedConstructor_TestCase_001, TestSize.Level0)
+{
+    Disk disk("disk-1", 4096, "sda1", USB_FLAG);
+    EXPECT_EQ(disk.GetDiskId(), "disk-1");
+    EXPECT_EQ(disk.GetSizeBytes(), 4096);
+    EXPECT_EQ(disk.GetDiskType(), USB_FLAG);
+    EXPECT_TRUE(disk.GetRemovable());
+    EXPECT_EQ(disk.GetSysPath(), "/dev/block/sda1");
+}
+
+HWTEST_F(DiskTest, ParameterizedConstructor_TestCase_002, TestSize.Level0)
+{
+    Disk disk("disk-ssd", 1024, "nvme0n1", DATA_DISK_SSD);
+    EXPECT_EQ(disk.GetDiskType(), DATA_DISK_SSD);
+    EXPECT_FALSE(disk.GetRemovable());
+    EXPECT_TRUE(disk.IsInternalDataDisk());
+}
+
+HWTEST_F(DiskTest, ParameterizedConstructor_TestCase_003, TestSize.Level0)
+{
+    Disk disk("disk-hdd", 2048, "sda", DATA_DISK_HDD);
+    EXPECT_EQ(disk.GetDiskType(), DATA_DISK_HDD);
+    EXPECT_FALSE(disk.GetRemovable());
+    EXPECT_TRUE(disk.IsInternalDataDisk());
+}
+
+HWTEST_F(DiskTest, GetDiskId_TestCase_001, TestSize.Level0)
+{
+    Disk disk("test-id", 0, "", DISK_TYPE_UNKNOWN);
+    EXPECT_EQ(disk.GetDiskId(), "test-id");
+}
+
+HWTEST_F(DiskTest, GetSizeBytes_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 999, "", DISK_TYPE_UNKNOWN);
+    EXPECT_EQ(disk.GetSizeBytes(), 999);
+}
+
+HWTEST_F(DiskTest, SetSizeBytes_TestCase_001, TestSize.Level0)
+{
+    Disk disk;
+    disk.SetSizeBytes(12345);
+    EXPECT_EQ(disk.GetSizeBytes(), 12345);
+}
+
+HWTEST_F(DiskTest, GetDiskType_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", CD_FLAG);
+    EXPECT_EQ(disk.GetDiskType(), CD_FLAG);
+}
+
+HWTEST_F(DiskTest, SetDiskType_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", USB_FLAG);
+    EXPECT_TRUE(disk.GetRemovable());
+    disk.SetDiskType(DATA_DISK_SSD);
+    EXPECT_EQ(disk.GetDiskType(), DATA_DISK_SSD);
+    EXPECT_FALSE(disk.GetRemovable());
+}
+
+HWTEST_F(DiskTest, SetDiskType_TestCase_002, TestSize.Level0)
+{
+    Disk disk("d", 0, "", DATA_DISK_SSD);
+    EXPECT_FALSE(disk.GetRemovable());
+    disk.SetDiskType(USB_FLAG);
+    EXPECT_TRUE(disk.GetRemovable());
+}
+
+HWTEST_F(DiskTest, GetRemovable_TestCase_001, TestSize.Level0)
+{
+    Disk diskUsb("d", 0, "", USB_FLAG);
+    EXPECT_TRUE(diskUsb.GetRemovable());
+    Disk diskSsd("d", 0, "", DATA_DISK_SSD);
+    EXPECT_FALSE(diskSsd.GetRemovable());
+}
+
+HWTEST_F(DiskTest, IsRemovable_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", SD_FLAG);
+    EXPECT_TRUE(disk.IsRemovable());
+    EXPECT_EQ(disk.IsRemovable(), disk.GetRemovable());
+}
+
+HWTEST_F(DiskTest, SetVolumeIds_TestCase_001, TestSize.Level0)
+{
+    Disk disk;
+    std::vector<std::string> ids = {"vol-1", "vol-2"};
+    disk.SetVolumeIds(ids);
+    EXPECT_EQ(disk.GetVolumeIds().size(), 2u);
+    EXPECT_EQ(disk.GetVolumeIds()[0], "vol-1");
+}
+
+HWTEST_F(DiskTest, SetVolumeIds_Move_TestCase_001, TestSize.Level0)
+{
+    Disk disk;
+    std::vector<std::string> ids = {"vol-a", "vol-b"};
+    disk.SetVolumeIds(std::move(ids));
+    EXPECT_EQ(disk.GetVolumeIds().size(), 2u);
+    EXPECT_EQ(disk.GetVolumeIds()[1], "vol-b");
+}
+
+HWTEST_F(DiskTest, GetVolumeIds_Default_TestCase_001, TestSize.Level0)
+{
+    Disk disk;
+    EXPECT_EQ(disk.GetVolumeIds().size(), 0u);
+}
+
+HWTEST_F(DiskTest, SetExtraInfo_TestCase_001, TestSize.Level0)
+{
+    Disk disk;
+    disk.SetExtraInfo("extra-data");
+    EXPECT_EQ(disk.GetExtraInfo(), "extra-data");
+}
+
+HWTEST_F(DiskTest, GetExtraInfo_Default_TestCase_001, TestSize.Level0)
+{
+    Disk disk;
+    EXPECT_EQ(disk.GetExtraInfo(), "");
+}
+
+HWTEST_F(DiskTest, GetSysPath_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "block1", USB_FLAG);
+    EXPECT_EQ(disk.GetSysPath(), "/dev/block/block1");
+}
+
+HWTEST_F(DiskTest, IsInternalDataDisk_TestCase_001, TestSize.Level0)
+{
+    Disk diskSsd("d", 0, "", DATA_DISK_SSD);
+    EXPECT_TRUE(diskSsd.IsInternalDataDisk());
+    Disk diskHdd("d", 0, "", DATA_DISK_HDD);
+    EXPECT_TRUE(diskHdd.IsInternalDataDisk());
+    Disk diskUsb("d", 0, "", USB_FLAG);
+    EXPECT_FALSE(diskUsb.IsInternalDataDisk());
+    Disk diskSd("d", 0, "", SD_FLAG);
+    EXPECT_FALSE(diskSd.IsInternalDataDisk());
+    Disk diskCd("d", 0, "", CD_FLAG);
+    EXPECT_FALSE(diskCd.IsInternalDataDisk());
+}
+
+HWTEST_F(DiskTest, UpdateRemovableFromDiskType_TestCase_001, TestSize.Level0)
+{
+    Disk disk;
+    EXPECT_TRUE(disk.GetRemovable());
+    disk.SetDiskType(DATA_DISK_SSD);
+    EXPECT_FALSE(disk.GetRemovable());
+    disk.SetDiskType(DATA_DISK_HDD);
+    EXPECT_FALSE(disk.GetRemovable());
+    disk.SetDiskType(USB_FLAG);
+    EXPECT_TRUE(disk.GetRemovable());
+    disk.SetDiskType(DISK_TYPE_UNKNOWN);
+    EXPECT_TRUE(disk.GetRemovable());
+}
+
+HWTEST_F(DiskTest, RefreshClassification_DvrUsb_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", DVR_USB);
+    disk.RefreshClassificationFromSysfs("/sys/devices/usb/test");
+    EXPECT_EQ(disk.GetDiskType(), DVR_USB);
+    EXPECT_TRUE(disk.GetRemovable());
+}
+
+HWTEST_F(DiskTest, RefreshClassification_CdFlag_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", CD_FLAG);
+    disk.RefreshClassificationFromSysfs("/sys/devices/block/sr0");
+    EXPECT_EQ(disk.GetDiskType(), CD_FLAG);
+}
+
+HWTEST_F(DiskTest, RefreshClassification_OpticalBlock_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", USB_FLAG);
+    disk.RefreshClassificationFromSysfs("/sys/devices/block/sr0/device");
+    EXPECT_EQ(disk.GetDiskType(), CD_FLAG);
+}
+
+HWTEST_F(DiskTest, RefreshClassification_Nvme_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", USB_FLAG);
+    disk.RefreshClassificationFromSysfs("/sys/devices/nvme/nvme0");
+    EXPECT_EQ(disk.GetDiskType(), DATA_DISK_SSD);
+    EXPECT_FALSE(disk.GetRemovable());
+}
+
+HWTEST_F(DiskTest, RefreshClassification_DirectSata_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", USB_FLAG);
+    disk.RefreshClassificationFromSysfs("/sys/devices/ata/ahci0");
+    EXPECT_EQ(disk.GetDiskType(), DATA_DISK_HDD);
+    EXPECT_FALSE(disk.GetRemovable());
+}
+
+HWTEST_F(DiskTest, RefreshClassification_DirectSata2_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", USB_FLAG);
+    disk.RefreshClassificationFromSysfs("/sys/devices/.sata/host0");
+    EXPECT_EQ(disk.GetDiskType(), DATA_DISK_HDD);
+}
+
+HWTEST_F(DiskTest, RefreshClassification_Usb_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", DISK_TYPE_UNKNOWN);
+    disk.RefreshClassificationFromSysfs("/sys/devices/usb/usb1");
+    EXPECT_EQ(disk.GetDiskType(), USB_FLAG);
+    EXPECT_TRUE(disk.GetRemovable());
+}
+
+HWTEST_F(DiskTest, RefreshClassification_UsbHost_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", DISK_TYPE_UNKNOWN);
+    disk.RefreshClassificationFromSysfs("/sys/devices/usbhost/1-1");
+    EXPECT_EQ(disk.GetDiskType(), USB_FLAG);
+}
+
+HWTEST_F(DiskTest, RefreshClassification_HiUsb_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", DISK_TYPE_UNKNOWN);
+    disk.RefreshClassificationFromSysfs("/sys/devices/hiusb/hiusb-otg");
+    EXPECT_EQ(disk.GetDiskType(), USB_FLAG);
+}
+
+HWTEST_F(DiskTest, RefreshClassification_PlatformHiUsb_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", DISK_TYPE_UNKNOWN);
+    disk.RefreshClassificationFromSysfs("/sys/devices/platform/hiusb/hiusb-otg");
+    EXPECT_EQ(disk.GetDiskType(), USB_FLAG);
+}
+
+HWTEST_F(DiskTest, RefreshClassification_Ehci_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", DISK_TYPE_UNKNOWN);
+    disk.RefreshClassificationFromSysfs("/sys/devices/.ehci/ehci0");
+    EXPECT_EQ(disk.GetDiskType(), USB_FLAG);
+}
+
+HWTEST_F(DiskTest, RefreshClassification_Ehci2_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", DISK_TYPE_UNKNOWN);
+    disk.RefreshClassificationFromSysfs("/sys/devices/ehci/ehci-hcd");
+    EXPECT_EQ(disk.GetDiskType(), USB_FLAG);
+}
+
+HWTEST_F(DiskTest, RefreshClassification_Ehci3_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", DISK_TYPE_UNKNOWN);
+    disk.RefreshClassificationFromSysfs("/sys/devices/ehci/ehci_hcd.0");
+    EXPECT_EQ(disk.GetDiskType(), USB_FLAG);
+}
+
+HWTEST_F(DiskTest, RefreshClassification_Xhci_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", DISK_TYPE_UNKNOWN);
+    disk.RefreshClassificationFromSysfs("/sys/devices/xhci/xhci-hcd");
+    EXPECT_EQ(disk.GetDiskType(), USB_FLAG);
+}
+
+HWTEST_F(DiskTest, RefreshClassification_Dwc3_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", DISK_TYPE_UNKNOWN);
+    disk.RefreshClassificationFromSysfs("/sys/devices/.dwc3/dwc3.0");
+    EXPECT_EQ(disk.GetDiskType(), USB_FLAG);
+}
+
+HWTEST_F(DiskTest, RefreshClassification_Dwc3Path_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", DISK_TYPE_UNKNOWN);
+    disk.RefreshClassificationFromSysfs("/sys/devices/dwc3/dwc3.0");
+    EXPECT_EQ(disk.GetDiskType(), USB_FLAG);
+}
+
+HWTEST_F(DiskTest, RefreshClassification_MmcHost_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", DISK_TYPE_UNKNOWN);
+    disk.RefreshClassificationFromSysfs("/sys/devices/mmc_host/mmc0");
+    EXPECT_EQ(disk.GetDiskType(), SD_FLAG);
+    EXPECT_TRUE(disk.GetRemovable());
+}
+
+HWTEST_F(DiskTest, RefreshClassification_Dwmmc_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", DISK_TYPE_UNKNOWN);
+    disk.RefreshClassificationFromSysfs("/sys/devices/dwmmc0");
+    EXPECT_EQ(disk.GetDiskType(), SD_FLAG);
+}
+
+HWTEST_F(DiskTest, RefreshClassification_HiMci_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", DISK_TYPE_UNKNOWN);
+    disk.RefreshClassificationFromSysfs("/sys/devices/hi_mci.0");
+    EXPECT_EQ(disk.GetDiskType(), SD_FLAG);
+}
+
+HWTEST_F(DiskTest, RefreshClassification_DwMmc_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", DISK_TYPE_UNKNOWN);
+    disk.RefreshClassificationFromSysfs("/sys/devices/dw_mmc/host0");
+    EXPECT_EQ(disk.GetDiskType(), SD_FLAG);
+}
+
+HWTEST_F(DiskTest, RefreshClassification_EmptySysPath_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", USB_FLAG);
+    disk.RefreshClassificationFromSysfs("");
+    EXPECT_EQ(disk.GetDiskType(), USB_FLAG);
+}
+
+HWTEST_F(DiskTest, RefreshClassification_EmptySysPath_Unknown_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", DISK_TYPE_UNKNOWN);
+    disk.RefreshClassificationFromSysfs("");
+    EXPECT_EQ(disk.GetDiskType(), DISK_TYPE_UNKNOWN);
+}
+
+HWTEST_F(DiskTest, RefreshClassification_EmptySysPath_Sd_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", SD_FLAG);
+    disk.RefreshClassificationFromSysfs("");
+    EXPECT_EQ(disk.GetDiskType(), SD_FLAG);
+}
+
+HWTEST_F(DiskTest, RefreshClassification_EmptySysPath_Cd_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", CD_FLAG);
+    disk.RefreshClassificationFromSysfs("");
+    EXPECT_EQ(disk.GetDiskType(), CD_FLAG);
+}
+
+HWTEST_F(DiskTest, RefreshClassification_NoMatch_TestCase_001, TestSize.Level0)
+{
+    Disk disk("d", 0, "", DISK_TYPE_UNKNOWN);
+    disk.RefreshClassificationFromSysfs("/sys/devices/virtual/block/loop0");
+    EXPECT_EQ(disk.GetDiskType(), DISK_TYPE_UNKNOWN);
+}
+
+HWTEST_F(DiskTest, Marshalling_Success_TestCase_001, TestSize.Level0)
+{
+    Disk disk("disk-1", 4096, "sda1", USB_FLAG);
+    disk.SetVolumeIds({"vol-1", "vol-2"});
+    disk.SetExtraInfo("extra");
+    Parcel parcel;
+    EXPECT_TRUE(disk.Marshalling(parcel));
+}
+
+HWTEST_F(DiskTest, Unmarshalling_Success_TestCase_001, TestSize.Level0)
+{
+    Disk disk("disk-1", 4096, "sda1", USB_FLAG);
+    disk.SetVolumeIds({"vol-1"});
+    disk.SetExtraInfo("extra-info");
+    Parcel parcel;
+    EXPECT_TRUE(disk.Marshalling(parcel));
+    Disk *result = Disk::Unmarshalling(parcel);
+    ASSERT_NE(result, nullptr);
+    EXPECT_EQ(result->GetDiskId(), "disk-1");
+    EXPECT_EQ(result->GetSizeBytes(), 4096);
+    EXPECT_EQ(result->GetDiskType(), USB_FLAG);
+    EXPECT_TRUE(result->GetRemovable());
+    EXPECT_EQ(result->GetVolumeIds().size(), 1u);
+    EXPECT_EQ(result->GetVolumeIds()[0], "vol-1");
+    EXPECT_EQ(result->GetExtraInfo(), "extra-info");
+    delete result;
+}
+
+HWTEST_F(DiskTest, Unmarshalling_EmptyVolumeIds_TestCase_001, TestSize.Level0)
+{
+    Disk disk("disk-2", 0, "", SD_FLAG);
+    Parcel parcel;
+    EXPECT_TRUE(disk.Marshalling(parcel));
+    Disk *result = Disk::Unmarshalling(parcel);
+    ASSERT_NE(result, nullptr);
+    EXPECT_EQ(result->GetVolumeIds().size(), 0u);
+    delete result;
+}
+
+HWTEST_F(DiskTest, Unmarshalling_OverMaxVolumeIds_TestCase_001, TestSize.Level0)
+{
+    Parcel parcel;
+    parcel.WriteString("disk-id");
+    parcel.WriteInt64(0);
+    parcel.WriteInt32(USB_FLAG);
+    parcel.WriteBool(true);
+    parcel.WriteUint32(257u);
+    Disk *result = Disk::Unmarshalling(parcel);
+    EXPECT_EQ(result, nullptr);
+}
+
+HWTEST_F(DiskTest, Marshalling_WriteDiskIdFail_TestCase_001, TestSize.Level0)
+{
+    Disk disk("disk-1", 4096, "sda1", USB_FLAG);
+    Parcel parcel;
+    parcel.WriteInt32(0);
+    EXPECT_FALSE(disk.Marshalling(parcel));
+}
+} // namespace DiskManager
+} // namespace OHOS
---
+++ test/unittest/volume_core_test.cpp
@@ -0,0 +1,203 @@
+/*
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include "volume_core.h"
+#include "message_parcel_mock.h"
+
+namespace OHOS {
+namespace DiskManager {
+
+using namespace testing;
+using namespace testing::ext;
+
+class VolumeCoreTest : public testing::Test {
+public:
+    static void SetUpTestCase(void) {}
+    static void TearDownTestCase(void) {}
+    void SetUp() override {}
+    void TearDown() override {}
+};
+
+HWTEST_F(VolumeCoreTest, DefaultConstructor_TestCase_001, TestSize.Level0)
+{
+    VolumeCore vc;
+    EXPECT_EQ(vc.GetId(), "");
+    EXPECT_EQ(vc.GetType(), 0);
+    EXPECT_EQ(vc.GetDiskId(), "");
+    EXPECT_EQ(vc.GetState(), UNMOUNTED);
+    EXPECT_EQ(vc.GetFsType(), "");
+}
+
+HWTEST_F(VolumeCoreTest, Constructor3Param_TestCase_001, TestSize.Level0)
+{
+    VolumeCore vc("vol-1", EXTERNAL, "disk-1");
+    EXPECT_EQ(vc.GetId(), "vol-1");
+    EXPECT_EQ(vc.GetType(), EXTERNAL);
+    EXPECT_EQ(vc.GetDiskId(), "disk-1");
+    EXPECT_EQ(vc.GetState(), UNMOUNTED);
+}
+
+HWTEST_F(VolumeCoreTest, Constructor4Param_TestCase_001, TestSize.Level0)
+{
+    VolumeCore vc("vol-2", EMULATED, "disk-2", MOUNTED);
+    EXPECT_EQ(vc.GetId(), "vol-2");
+    EXPECT_EQ(vc.GetType(), EMULATED);
+    EXPECT_EQ(vc.GetDiskId(), "disk-2");
+    EXPECT_EQ(vc.GetState(), MOUNTED);
+}
+
+HWTEST_F(VolumeCoreTest, Constructor5Param_TestCase_001, TestSize.Level0)
+{
+    VolumeCore vc("vol-3", EXTERNAL, "disk-3", CHECKING, "vfat");
+    EXPECT_EQ(vc.GetId(), "vol-3");
+    EXPECT_EQ(vc.GetType(), EXTERNAL);
+    EXPECT_EQ(vc.GetDiskId(), "disk-3");
+    EXPECT_EQ(vc.GetState(), CHECKING);
+    EXPECT_EQ(vc.GetFsType(), "vfat");
+}
+
+HWTEST_F(VolumeCoreTest, GetId_TestCase_001, TestSize.Level0)
+{
+    VolumeCore vc("test-id", 0, "");
+    EXPECT_EQ(vc.GetId(), "test-id");
+}
+
+HWTEST_F(VolumeCoreTest, GetType_TestCase_001, TestSize.Level0)
+{
+    VolumeCore vc("v", EMULATED, "d");
+    EXPECT_EQ(vc.GetType(), EMULATED);
+}
+
+HWTEST_F(VolumeCoreTest, GetDiskId_TestCase_001, TestSize.Level0)
+{
+    VolumeCore vc("v", 0, "my-disk");
+    EXPECT_EQ(vc.GetDiskId(), "my-disk");
+}
+
+HWTEST_F(VolumeCoreTest, GetState_TestCase_001, TestSize.Level0)
+{
+    VolumeCore vc("v", 0, "d", EJECTING);
+    EXPECT_EQ(vc.GetState(), EJECTING);
+}
+
+HWTEST_F(VolumeCoreTest, SetState_TestCase_001, TestSize.Level0)
+{
+    VolumeCore vc;
+    vc.SetState(MOUNTED);
+    EXPECT_EQ(vc.GetState(), MOUNTED);
+    vc.SetState(UNMOUNTED);
+    EXPECT_EQ(vc.GetState(), UNMOUNTED);
+}
+
+HWTEST_F(VolumeCoreTest, GetFsType_TestCase_001, TestSize.Level0)
+{
+    VolumeCore vc("v", 0, "d", 0, "ext4");
+    EXPECT_EQ(vc.GetFsType(), "ext4");
+}
+
+HWTEST_F(VolumeCoreTest, Marshalling_Success_TestCase_001, TestSize.Level0)
+{
+    VolumeCore vc("vol-1", EXTERNAL, "disk-1", MOUNTED, "vfat");
+    Parcel parcel;
+    EXPECT_TRUE(vc.Marshalling(parcel));
+}
+
+HWTEST_F(VolumeCoreTest, Unmarshalling_Success_TestCase_001, TestSize.Level0)
+{
+    VolumeCore vc("vol-1", EMULATED, "disk-1", MOUNTED, "ext4");
+    Parcel parcel;
+    EXPECT_TRUE(vc.Marshalling(parcel));
+    VolumeCore *result = VolumeCore::Unmarshalling(parcel);
+    ASSERT_NE(result, nullptr);
+    EXPECT_EQ(result->GetId(), "vol-1");
+    EXPECT_EQ(result->GetType(), EMULATED);
+    EXPECT_EQ(result->GetDiskId(), "disk-1");
+    EXPECT_EQ(result->GetState(), MOUNTED);
+    EXPECT_EQ(result->GetFsType(), "ext4");
+    delete result;
+}
+
+HWTEST_F(VolumeCoreTest, Marshalling_WriteIdFail_TestCase_001, TestSize.Level0)
+{
+    VolumeCore vc("vol-1", EXTERNAL, "disk-1", MOUNTED, "vfat");
+    Parcel parcel;
+    parcel.WriteInt32(0);
+    EXPECT_FALSE(vc.Marshalling(parcel));
+}
+
+class VolumeInfoStrTest : public testing::Test {
+public:
+    static void SetUpTestCase(void) {}
+    static void TearDownTestCase(void) {}
+    void SetUp() override {}
+    void TearDown() override {}
+};
+
+HWTEST_F(VolumeInfoStrTest, DefaultConstructor_TestCase_001, TestSize.Level0)
+{
+    VolumeInfoStr vis;
+    EXPECT_EQ(vis.volumeId, "");
+    EXPECT_EQ(vis.fsTypeStr, "");
+    EXPECT_EQ(vis.fsUuid, "");
+    EXPECT_EQ(vis.path, "");
+    EXPECT_EQ(vis.description, "");
+    EXPECT_FALSE(vis.isDamaged);
+}
+
+HWTEST_F(VolumeInfoStrTest, ParameterizedConstructor_TestCase_001, TestSize.Level0)
+{
+    VolumeInfoStr vis("vol-1", "vfat", "uuid-1", "/mnt", "desc", true);
+    EXPECT_EQ(vis.volumeId, "vol-1");
+    EXPECT_EQ(vis.fsTypeStr, "vfat");
+    EXPECT_EQ(vis.fsUuid, "uuid-1");
+    EXPECT_EQ(vis.path, "/mnt");
+    EXPECT_EQ(vis.description, "desc");
+    EXPECT_TRUE(vis.isDamaged);
+}
+
+HWTEST_F(VolumeInfoStrTest, Marshalling_Success_TestCase_001, TestSize.Level0)
+{
+    VolumeInfoStr vis("vol-1", "ext4", "uuid-1", "/data", "data", false);
+    Parcel parcel;
+    EXPECT_TRUE(vis.Marshalling(parcel));
+}
+
+HWTEST_F(VolumeInfoStrTest, Unmarshalling_Success_TestCase_001, TestSize.Level0)
+{
+    VolumeInfoStr vis("vol-1", "ext4", "uuid-1", "/data", "data", true);
+    Parcel parcel;
+    EXPECT_TRUE(vis.Marshalling(parcel));
+    VolumeInfoStr *result = VolumeInfoStr::Unmarshalling(parcel);
+    ASSERT_NE(result, nullptr);
+    EXPECT_EQ(result->volumeId, "vol-1");
+    EXPECT_EQ(result->fsTypeStr, "ext4");
+    EXPECT_EQ(result->fsUuid, "uuid-1");
+    EXPECT_EQ(result->path, "/data");
+    EXPECT_EQ(result->description, "data");
+    EXPECT_TRUE(result->isDamaged);
+    delete result;
+}
+
+HWTEST_F(VolumeInfoStrTest, Marshalling_WriteVolumeIdFail_TestCase_001, TestSize.Level0)
+{
+    VolumeInfoStr vis("vol-1", "vfat", "uuid", "/mnt", "desc", false);
+    Parcel parcel;
+    parcel.WriteInt32(0);
+    EXPECT_FALSE(vis.Marshalling(parcel));
+}
+} // namespace DiskManager
+} // namespace OHOS
