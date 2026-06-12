f10c543e321d78edc42dd81a5251348cefa05dce
15667096613 <zhangchenyang30@h-partners.com>
2026-06-12 17:27:34 +0800

    E_PERMISSION_DENIED错误码对齐storage_service，新增E_SYS_APP_PERMISSION_DENIED，移除冗余依赖及调试日志
    Signed-off-by: 15667096613 <zhangchenyang30@h-partners.com>

--- utils/src/ipc_caller_auth.cpp
+++ utils/src/ipc_caller_auth.cpp
@@ -58,7 +58,6 @@ bool IpcCallerAuth::IsCallingSystemApp()
     auto tokenType = AccessTokenKit::GetTokenTypeFlag(tokenCaller);
     if (tokenType == Security::AccessToken::TOKEN_HAP) {
         uint64_t fullTokenId = IPCSkeleton::GetCallingFullTokenID();
-        LOGI("IpcCallerAuth: IsCallingSystemApp fullTokenId=%{public}llu", static_cast<long long>(fullTokenId));
         return AccessTokenKit::IsSystemAppByFullTokenID(fullTokenId);
     }
     return true;
